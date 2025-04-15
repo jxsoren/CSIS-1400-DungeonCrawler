c67127733c396a5ae50e08dff722de41bf8fecd6
jxsoren <jxsoren@gmail.com>
2025-04-14 21:05:22 -0600

    added class to help w/ tests

---
+++ test/TestHelpers.java
@@ -0,0 +1,15 @@
+public class TestHelpers {
+
+    public static String assertionMessage(String subject, String expectedValue, String actualValue) {
+        return String.format("%s is expected to be %s, but is actually %s", subject, expectedValue, actualValue);
+    }
+
+    public static String assertionMessage(String subject, int expectedValue, int actualValue) {
+        return String.format("%s is expected to be %s, but is actually %s", subject, expectedValue, actualValue);
+    }
+
+    public static String assertionMessage(String expectedSubject, String expression, int actualValue) {
+        return String.format("%s is expected to be %s, but is actually %s", expectedSubject, actualValue);
+    }
+
+}
