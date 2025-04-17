647f3984a20599f24bf9013f8509299865648904
SlenderSwift <bowenski05@gmail.com>
2025-04-16 22:18:35 -0600

    added new AsciiArt file

---
+++ src/AsciiArt.java
@@ -0,0 +1,33 @@
+//will need to interact with the enemy class. we will have to create an enemy object. one of the methods of this object should include a printf statement.
+//this will print out the enemy/object/chest
+//we may need to make an AsciiLibrary class that has a list of these different artworks
+//
+public class AsciiArt {
+    public static void main(String[] args) {
+        Enemy enemy = new Enemy();
+        displayThing(enemy.displayEnemy());
+    }
+
+    public static void displayThing(String thing) {
+        System.out.printf("%s%n", "-".repeat(50));
+        // Split the string into lines and add 25 spaces to each line
+        String[] lines = thing.split("\n");
+        for (String line : lines) {
+            System.out.printf("%15s%s%n", "", line); // 25 spaces + line content
+        }
+        System.out.printf("%s%n%n", "-".repeat(50));
+    }
+}
+
+class Enemy {
+    public String displayEnemy() {
+        return  "   __________\n" +
+                "  /\\____;;___\\\n" +
+                " | /         /\n" +
+                "  .__________.\n" +
+                "  |\\ ________\\\n" +
+                "  | |---------|\n" +
+                "  \\ |         |\n" +
+                "   \\|_________|\n";
+    }
+}
