18256601720455b4c4153a23f82e7afd8e9f4a8c
jxsoren <jxsoren@gmail.com>
2025-04-18 16:44:37 -0600

    init classes to startup game

---
+++ src/PlayGame.java
@@ -0,0 +1,23 @@
+/**********************************************
+ * Author(s): Josh Sorensen & Bowen Berthelson
+ *
+ * Assignment: CSIS 1400 Final Project
+ ***********************************************/
+
+import java.util.Scanner;
+
+public class PlayGame {
+    public static void main(String[] args) {
+        Player player = initPlayer();
+        DungeonGame game = new DungeonGame(player);
+    }
+
+    private static Player initPlayer() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What's your name? ");
+        String playerName = input.nextLine();
+
+        return new Player(playerName);
+    }
+}
---
+++ test/PlayGameTest.java
@@ -0,0 +1,14 @@
+public class PlayGameTest {
+
+    public static void main(String[] args) {
+        try {
+            //            example();
+            System.out.println("example() test +PASSED+ !!!");
+        } catch (AssertionError e) {
+            System.err.printf("example() test -FAILED-. Error Message: %s", e.getMessage());
+        }
+
+    }
+
+
+}
