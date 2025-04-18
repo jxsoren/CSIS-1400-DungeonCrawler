1a29ac8fd195eb1589cbd67740425942766d6419
SlenderSwift <bowenski05@gmail.com>
2025-04-18 13:57:23 -0600

    added ascii for goblin, sword, and potion. modified the display to include player and enemy health

--- src/AsciiArt.java
+++ src/AsciiArt.java
@@ -13,6 +13,7 @@ public class AsciiArt {
         displayWindow(chest());
     }
 
+    //this method takes in a String from the methods at the bottom of the page
     public static void displayWindow(String thing) {
         System.out.printf("%s%n", "-".repeat(50));
         // Split the string into lines and add 25 spaces to each line
@@ -20,6 +21,14 @@ public class AsciiArt {
         for (String line : lines) {
             System.out.printf("%15s%s%n", "", line); // 25 spaces + line content
         }
+        System.out.printf("Your Health:%-21d Enemy Health:%d%n", Player.getCurrentHealth(), Enemy.getCurrentHealth());// will display player and enemy's health
+        System.out.printf("%s%n%n", "-".repeat(50));
+    }
+    //this overflow method will display the screen if no String is given
+    public static void displayWindow() {
+        System.out.printf("%s%n%n%n%n%n%n%n%n", "-".repeat(50));
+
+
         System.out.printf("%s%n%n", "-".repeat(50));
     }
 
@@ -33,5 +42,49 @@ public class AsciiArt {
                 "  \\ |         |\n" +
                 "   \\|_________|\n";
     }
+    public static String goblin(){
+        return  "       ,      ,\n" +
+                "     /(.-\"\"-.)\\\n" +
+                " |\\  \\/      \\/  /|\n" +
+                " | \\ / =.  .= \\ / |\n" +
+                " \\( \\   o\\/o   / )/\n" +
+                "  \\_, '-/  \\-' ,_/\n" +
+                "    /   \\__/   \\\n" +
+                "    \\ \\__/\\__/ /\n" +
+                "  ___\\ \\|--|/ /___\n" +
+                " /`   \\      /    `\\\n" +
+                "/      '----'       \\\n";
+    }
+    public static String sword(){
+        return  "        () \n" +
+                "      __)(__\n" +
+                "      '-<>-'\n" +
+                "        )( \n" +
+                "        || \n" +
+                "        || \n" +
+                "        || \n" +
+                "        || \n" +
+                "        || \n" +
+                "        || \n" +
+                "        || \n" +
+                "        || \n" +
+                "        \\/\n" ;
+    }
+    public static String potion(){
+        return  "      (------)\n" +
+                "       [ 0  ]\n" +
+                "       /0   \\\n" +
+                "      /    o \\\n" +
+                "     /  o   0 \\\n" +
+                "    /----------\\\n" +
+                "   /            \\\n" +
+                "  /              \\\n" +
+                " /                \\\n" +
+                "/                  \\\n" +
+                "[                  ]\n" +
+                "\\__________________/\n";
+
+    }
+
 
 }
